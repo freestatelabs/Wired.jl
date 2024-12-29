5f4b091c81414ba8db26b9ded7bd5f2cde338f85
freestatelabs <freestatelabs@gmail.com>
2024-12-29 15:09:48 -0500

    added validation example of finite wire

--- docs/src/validation.md
+++ docs/src/validation.md
@@ -2,7 +2,38 @@
 
 Code validation for `Wired.jl`.
 
-## Example Problem 
+## Infinite Wire with Finite Radius
+
+Given an infinite wire along the z-axis with the following parameters:
+- `r = 10 mm` (radius of cross-section)
+- `I = 1 kA` 
+
+Calculate the y-component of the magnetic field along the x-axis.  The analytical solution is provided by:  
+$$B_y(x) = \frac{\mu \space I}{2\pi\space r^2}\cdot x, \space (x<=r)$$
+$$B_y(x) = \frac{\mu \space I}{2pi \cdot x}, \space (x > r)$$
+
+### Code
+```julia
+mu = 1.0; I = 1000; r = 1.0;
+wire = Wire([0, 0, -1e4], [0, 0, 1e4], I, r)
+line = Line([0,0,0], [2, 0, 0], 100)
+
+By = bfield(line.nodes, [wire])[:,2]
+Banalytical = map(x -> (x > r) ? mu0*I/(2pi*x) : mu0*I*x/(2pi*r^2), line.nodes[:,1])
+
+using Plots 
+p = plot()
+plot!(p, line.nodes[:,1], Banalytical, label="Analytical")
+plot!(p, line.nodes[1:5:end,1], By[1:5:end], label="Wire", linealpha=0, markershape=:circle)
+xlabel!(p, "Distance Along X-Axis [m]")
+ylabel!(p, "Magnetic Flux Density (By) [T]")
+```
+
+### Result
+
+![Validation: Infinite Wire, Finite Radius](figs/validation-wire.svg)
+
+## Circular Ring with Finite Radius
 
 Given a circular curent-carrying ring with a circular cross-section and the following parameters:
 - `H = 0.0 m`
@@ -19,7 +50,7 @@ With the following methods
 - Using a `Wired.CircularRing` primitive 
 - Using a finite element mesh
 
-## Code
+### Code
 
 ```julia
 H = 0.0 	# height above xy plane
@@ -37,6 +68,6 @@ Bz_ring = bfield(zaxis.nodes, [ring])
 Bz_analytical = analytical.(zaxis.nodes[:,3])
 ```
 
-## Results
+### Result
 
 ![Z-Axis Results](figs/validation-zaxis.svg)
--- test/validation.jl
+++ test/validation.jl
@@ -4,7 +4,8 @@
 """
 
 using Wired 
-make_plots = false 
+using Printf
+make_plots = true
 
 if make_plots	
 	# Don't want to make Plots a dependency
@@ -12,7 +13,7 @@ if make_plots
 end
 
 
-function test_validation(do_plot=false, savefigs=false)
+function test_validation(make_plots=false, savefigs=false)
 
 	H = 0.0 
 	R = 2.0 
@@ -35,7 +36,7 @@ function test_validation(do_plot=false, savefigs=false)
 	By_ring = bfield(xaxis.nodes, [ring])
 	By_analytical = analytical.(xaxis.nodes[:,1])
 
-	if do_plot
+	if make_plots
 		p = plot(zaxis.nodes[:,3], Bz_analytical, label="Analytical", color=:black)
 		plot!(p, zaxis.nodes[1:50:1000,3], Bz_ring[1:50:1000,3], label="Ring Primitive", markershape=:circle, markercolor=:red, markeralpha=1.0, linetype=:scatter)
 		xlabel!("Distance Along Z-axis [m]")
@@ -61,5 +62,44 @@ function test_validation(do_plot=false, savefigs=false)
 	else
 		return false
 	end 
+
+end
+
+
+function test_wireconvergence(Nwires; H=0, R=1, r=0.01, I=1e3)
+	# Test how many wires are required to converge to ring solution 
+
+	ring = CircularRing("Ring", H, R, r, I)
+
+	# Discretize the ring into `Wire` segments
+	wires = Vector{Wire{Float64}}(undef, Nwires)
+	dtheta = (2pi)/(Nwires-1)
+	theta = dtheta
+	wires[1] = Wire{Float64}([R,0,0],[R*cos(theta), R*sin(theta), 0], I, r)
+	for i in 2:Nwires 
+		x0 = wires[i-1].a1[1] 
+		y0 = wires[i-1].a1[2]
+		wires[i] = Wire{Float64}([x0, y0, 0], [R*cos(theta),R*sin(theta),0], I, r)
+		theta += dtheta
+	end
+
+	line = Line([0,0,0], [2,0,0], 1000)
+	Bring = bfield(line.nodes, [ring], errmax=1e-16)
+	Bwires = bfield(line.nodes, wires)
+
+	@printf "Max/min Bring = %.3e/%.3e T\n" maximum(Bring) minimum(Bring)
+	@printf "Max/min Bwires = %.3e/%.3e T\n" maximum(Bwires) minimum(Bwires)
+	errmax = 100*(maximum(Bring) - maximum(Bwires)) / maximum(Bwires)
+	errmin = 100*(minimum(Bring) - minimum(Bwires)) / minimum(Bwires)
+	@printf "Rel error max/min = %.0f%%/%.0f%%" errmax errmin
+
+	if make_plots
+		p = plot()
+		plot!(p, line.nodes[:,1], Bring[:,3], label="Bring")
+		plot!(p, line.nodes[:,1], Bwires[:,3], label="Bwires")
+		xlabel!("Distance from central axis [m]")
+		ylabel!("Magnetic Flux Density (Bz) [T]")
+		display(p)
+	end
 
 end
